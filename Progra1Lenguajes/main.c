57ae221a9296d8e6b6ddab7c2ceb88056a142fbe
danie <danie@LAPTOP-VB4EU9DH>
2019-03-31 20:10:08 -0600

    Funcion para agregar empleado creada

--- Progra1Lenguajes/main.c
+++ Progra1Lenguajes/main.c
@@ -3,38 +3,61 @@
 #include <stdlib.h>
 
 //Estructura donde se guardan temporalmente los datos de empleados
-typedef struct nodo{
+typedef struct nodoEmp{
     int codigoEmpleado;
-    char nombre[];
-    char apellido[];
-    char departamento[];
+    int nombre[50];
+    char apellido[50];
+    char departamento[50];
     int codigoProyecto;
-    nodo *sig;
+    struct nodoEmp* sig;
 }empleado;
 empleado *cabezaEmp=NULL;
 
 //Estructura donde se guardan temporalmente los datos de proyectos
-typedef struct nodo{
+typedef struct nodoPro{
     int codigoProyecto;
-    char nombre[];
+    char nombre[50];
     int anioInicio;
     int anioFin;
-    nodo *sig;
+    struct nodoPro* sig;
 }proyecto;
 proyecto *cabezaPro=NULL;
 
 //Estructura donde se guardan temporalmente los datos de actividades
-typedef struct nodo{
+typedef struct nodoAct{
     int codigoActividad;
-    char descripcion[];
+    char descripcion[50];
     int avance;
     int codigoProyecto;
-    nodo*sig;
+    struct nodoAct* sig;
 }actividad;
 actividad *cabezaAct= NULL;
 
 void agregarEmpleado(){
+    //TODAVIA FALTA LO DE CODIGO EMPLEADO, ESTE SE DEBE DE GENERAR AUTOMATICAMENTE
     
+    //SE CREA NUEVO NODO TIPO EMPLEADO Y SE LE AGREGAN SUS DATOS
+    empleado* nuevo=(empleado*)malloc(sizeof(nuevo));
+    printf("Digite su nombre\n");
+    scanf("%s",&nuevo->nombre);
+    printf("Digite su apellido\n");
+    scanf("%s",&nuevo->apellido);
+    printf("Digite su departamento\n");
+    scanf("%s",&nuevo->departamento);
+    
+    //ESTOS LOS PUSE NADA MAS PARA PROBAR
+    nuevo->codigoEmpleado=1;
+    nuevo->codigoProyecto=1;
+    
+    //Si la pila esta vacia se agrega en el tope y si no, se coloca encima del que sea el tope
+    if(cabezaEmp==NULL){
+        cabezaEmp=nuevo;
+        cabezaEmp->sig=NULL;
+    }
+    else{
+        nuevo->sig=cabezaEmp;
+        cabezaEmp=nuevo;
+    }
 }
 
 void agregarProyecto(){
@@ -80,24 +103,21 @@ int main() {
     while(estado==1){
         //Se le pide al usuario que digite una de las opciones
         printf("\nSelecciones alguna de las opciones\n");
-        printf("1 para agregar\n2 para eliminar\n3 para tamanio\n4 para imprimir\n5 para consultar valor en top\n6 para salir\n");
+        printf("1 para agregar\n2 para modificar actividad\n3 para consultar proyecto\n4 para consultar empleado\n5 para salir\n");
         scanf("%d",&opcion);
         switch(opcion){
-                //Si elige la opcion 1, se agrega un numero, y ademas se suma 1 a variable longitud para tener control del tamano de la pila
-                case 1: agregar();
-                        break;
-                //Si elige la opcion 2, se elimina el numero en tope y se le resta 1 a variable longitud
-                case 2: modificarActividad();
-                        break;
-                case 3: consultarProyecto();
-                        break;
-                case 4: consultarEmpleado();
-                        break;
-                case 5:	estado=0;
-                        break;
-                default: printf("Utilice solo las opciones validas\n");
-            }
-
+            case 1: agregar();
+                    break;
+            case 2: modificarActividad();
+                    break;
+            case 3: consultarProyecto();
+                    break;
+            case 4: consultarEmpleado();
+                    break;
+            case 5: estado=0;
+                    break;
+            default: printf("Utilice solo las opciones validas\n");
+        }
     }
     printf("Ejecucion terminada\n");
     return 0;
